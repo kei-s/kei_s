d3e81a24c3304e3347f4a4ab06ecef5216f2cb69
kei <kei-s@mbc.nifty.com>
2011-02-24 19:07:31 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeiS::Application.config.secret_token = 'f8a13bccdf43a90722548b253665399e965792b5ad919c5276d6dc107b8386630ed0445c2a796defa34c7a3bcd44f2daa0f7205fbf76ef52e50d65cb27b93e0f'
